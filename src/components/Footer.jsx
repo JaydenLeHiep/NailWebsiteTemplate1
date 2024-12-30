32c46c41ac0f77dd85814685a55faf6035ced96e
JaydenLeHiep <98736015+JaydenLeHiep@users.noreply.github.com>
2024-12-30 15:04:54 +0100

    update: fix styles of navbar and footer and contents

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,21 +11,8 @@ const Footer = () => {
           <ul>
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
-            <li><a href="#menu">Menu</a></li>
-            <li><a href="#order">Order</a></li>
             <li><a href="#service">Service</a></li>
             <li><a href="#contact">Contact</a></li>
-          </ul>
-        </div>
-
-        <div className="footer-section">
-          <h4>Our Services</h4>
-          <ul>
-            <li><a href="#">Web Development</a></li>
-            <li><a href="#">Product Management</a></li>
-            <li><a href="#">Food Delivery</a></li>
-            <li><a href="#">Quality Food</a></li>
-            <li><a href="#">Cart Service</a></li>
           </ul>
         </div>
 
@@ -43,10 +30,7 @@ const Footer = () => {
           <h4>Contact Us</h4>
           <div className="social-icons">
             <a href="https://www.facebook.com"><BsFacebook /></a>
-            <a href="https://www.twitter.com"><BsTwitter /></a>
             <a href="https://www.instagram.com"><BsInstagram /></a>
-            <a href="https://www.youtube.com"><BsYoutube /></a>
-            <a href="https://www.github.com"><BsGithub /></a>
           </div>
         </div>
       </div>
